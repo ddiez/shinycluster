dd6edd19a0c45f7154e8dcd167a87f8971209e3d
Diego Diez <diego10ruiz@gmail.com>
2018-10-19 13:30:47 +0900

    Reorganize code into separate files.

---
+++ R/ui.R
@@ -0,0 +1,30 @@
+ui_fun <- function() {
+  ui <- fluidPage(
+    useShinyjs(),
+    titlePanel("Select clusters"),
+
+    sidebarLayout(
+      sidebarPanel(
+        actionButton("add", "Add", width = 100),
+        disabled(actionButton("undo", "Undo", width = 100)),
+        disabled(actionButton("clear", "Clear", width = 100)),
+        hr(),
+        uiOutput("ui.xvar"),
+        uiOutput("ui.yvar"),
+        uiOutput("ui.color"),
+        sliderInput("size", "size", min = .1, max = 3, value = 1, step = .1, ticks = FALSE, width = 200),
+        hr(),
+        h3("Debug info"),
+        verbatimTextOutput("ncluster"),
+        verbatimTextOutput("info"),
+        verbatimTextOutput("debug")
+      ),
+
+      mainPanel(
+        plotOutput("plot", click = "plot_click", dblclick = "plot_dblclick"),
+        dataTableOutput("table")
+      )
+    )
+  )
+  ui
+}
